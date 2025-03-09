c9bf1e29e10fca88c9ebcef6f60934d80cb23e91
Warren <warren@isb>
2025-03-10 00:18:41 +0400

    Bugfix

--- py/modules/__init__.py
+++ py/modules/__init__.py
@@ -1,4 +1,4 @@
 # from modules.pose_encoder import PoseEncoder # type: ignore
-from modules.attn_processors import PlusAttnProcessor2_0, SkipAttnProcessor2_0  # type: ignore
+from .attn_processors import PlusAttnProcessor2_0, SkipAttnProcessor2_0  # type: ignore
 # from modules.unet import UNet2DConditionModel
-from modules.cloth_masker import AutoMasker
+from .cloth_masker import AutoMasker
